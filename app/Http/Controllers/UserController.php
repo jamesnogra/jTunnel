43b125a7caf31246e673c2483a2d5e6b9a0c8a35
James Arnold <love.gerald@yahoo.com>
2016-03-26 11:20:25 +0800

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -62,7 +62,7 @@ class UserController extends Controller
         // Always set content-type when sending HTML email
         $headers = "MIME-Version: 1.0" . "\r\n";
         $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-        $headers .= 'From: <no-reply@jtunnel.therookieblog.com>' . "\r\n";
+        $headers .= 'From: jTunnel Admin<no-reply@jtunnel.therookieblog.com>' . "\r\n";
 
         mail($to,$subject,$message,$headers);
     }
